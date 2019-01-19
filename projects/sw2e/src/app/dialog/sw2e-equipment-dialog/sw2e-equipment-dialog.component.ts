8a5efdc848d452700106396f0f41f14010b1e205
Gabor Heves <hevesg@gmail.com>
2019-01-19 15:08:27 +0100

    fix modal css

--- projects/sw2e/src/app/dialog/sw2e-equipment-dialog/sw2e-equipment-dialog.component.ts
+++ projects/sw2e/src/app/dialog/sw2e-equipment-dialog/sw2e-equipment-dialog.component.ts
@@ -9,13 +9,48 @@ import {Sw2eInventory} from '../../interface/sw2e-inventory';
 })
 export class Sw2eEquipmentDialogComponent {
 
+  public dialogData: Sw2eInventory;
+
   constructor(
     public dialogRef: MatDialogRef<Sw2eEquipmentDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Sw2eInventory
-  ) { }
+  ) {
+    console.log('const');
+    this.dialogData = Object.assign({}, this.data);
+  }
 
   public close() {
     this.dialogRef.close();
+  }
+
+  public save() {
+    console.log(this.dialogData);
+    this.dialogRef.close();
+  }
+
+  public addEquipment() {
+
+  }
+
+  public removeEquipment(index: number): void {
+    this.dialogData.equipment.splice(index, 1);
+    console.log(this.data);
+    console.log(this.dialogData);
+  }
+
+  public addItem(index: number): void {
+    let quantity = this.dialogData.equipment[index].quantity ? this.dialogData.equipment[index].quantity : 1;
+    quantity++;
+    this.dialogData.equipment[index].quantity = quantity;
+  }
+
+  public removeItem(index: number): void {
+    if (this.dialogData.equipment[index].quantity > 1) {
+      this.dialogData.equipment[index].quantity--;
+    }
+    if (this.dialogData.equipment[index].quantity === 1) {
+      delete this.dialogData.equipment[index].quantity;
+    }
   }
 
 }

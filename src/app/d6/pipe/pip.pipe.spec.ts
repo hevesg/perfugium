48fffab6a11df3e7c3f908b6b083af2014f84d52
Gabor Heves <hevesg@gmail.com>
2019-02-20 22:21:54 +0100

    PipPipe test

--- src/app/d6/pipe/pip.pipe.spec.ts
+++ src/app/d6/pipe/pip.pipe.spec.ts
@@ -1,8 +1,25 @@
 import { PipPipe } from './pip.pipe';
 
 describe('PipPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PipPipe();
-    expect(pipe).toBeTruthy();
+
+  let pipe: PipPipe;
+  beforeEach(() => {
+    pipe = new PipPipe();
+  });
+
+  it('should transform 0 to 0D', () => {
+    expect(pipe.transform(0)).toBe('0D');
+  });
+
+  it('should transform 3 to 1D', () => {
+    expect(pipe.transform(3)).toBe('1D');
+  });
+
+  it('should transform 10 to 3D+1', () => {
+    expect(pipe.transform(10)).toBe('3D+1');
+  });
+
+  it('should transform 11 to 3D+2', () => {
+    expect(pipe.transform(11)).toBe('3D+2');
   });
 });

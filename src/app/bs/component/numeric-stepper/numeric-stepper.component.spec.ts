785383af16569db1aec6b3576b792761c1f5e6de
Gabor Heves <hevesg@gmail.com>
2019-02-20 22:36:02 +0100

    NumericStepper tests

--- src/app/bs/component/numeric-stepper/numeric-stepper.component.spec.ts
+++ src/app/bs/component/numeric-stepper/numeric-stepper.component.spec.ts
@@ -19,7 +19,31 @@ describe('NumericStepperComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should increase value', () => {
+    expect(component.maxDisabled).toBeFalsy();
+    component.increase();
+    expect(component.value).toEqual(1);
+  });
+
+  it('should decrease value', () => {
+    expect(component.minDisabled).toBeFalsy();
+    component.decrease();
+    expect(component.value).toEqual(-1);
+  });
+
+  it('should not increase value if max is reached', () => {
+    component.max = 0;
+    expect(component.maxDisabled).toBeTruthy();
+
+    component.increase();
+    expect(component.value).toEqual(0);
+  });
+
+  it('should not decrease value if min is reached', () => {
+    component.min = 0;
+    expect(component.minDisabled).toBeTruthy();
+
+    component.decrease();
+    expect(component.value).toEqual(0);
   });
 });

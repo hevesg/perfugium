aa6fe57e8e41aafb217966ef425deb4218994ac6
Gabor Heves <hevesg@gmail.com>
2019-02-22 23:59:18 +0100

    removed equipment and weapons components

--- projects/sw2e/src/app/app.module.ts
+++ projects/sw2e/src/app/app.module.ts
@@ -6,8 +6,6 @@ import { AppComponent } from './app.component';
 import {Sw2eCharacterListComponent} from './component/sw2e-character-list/sw2e-character-list.component';
 import {D6Module} from '../../../../src/app/d6/d6.module';
 import {Sw2eCharacterSheetComponent} from './component/sw2e-character-sheet/sw2e-character-sheet.component';
-import {Sw2eEquipmentComponent} from './component/sw2e-equipment/sw2e-equipment.component';
-import {Sw2eWeaponsComponent} from './component/sw2e-weapons/sw2e-weapons.component';
 import {Sw2eEquipmentDialogComponent} from './dialog/sw2e-equipment-dialog/sw2e-equipment-dialog.component';
 
 @NgModule({
@@ -15,8 +13,6 @@ import {Sw2eEquipmentDialogComponent} from './dialog/sw2e-equipment-dialog/sw2e-
     AppComponent,
     Sw2eCharacterListComponent,
     Sw2eCharacterSheetComponent,
-    Sw2eEquipmentComponent,
-    Sw2eWeaponsComponent,
     Sw2eEquipmentDialogComponent
   ],
   imports: [

0d23ac2490df122f3cc438344f7ecd98c4173e85
Gabor Heves <hevesg@gmail.com>
2021-01-28 20:54:02 +0100

    created d6 library

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,13 @@ const { compilerOptions } = require('./tsconfig');
 module.exports = {
   preset: 'jest-preset-angular',
   roots: [
-    '<rootDir>/projects/sw2e/src/'
+    '<rootDir>/projects/sw2e/src/',
+    '<rootDir>/projects/d6/src/',
   ],
   testMatch: ['**/+(*.)+(spec).+(ts)'],
   setupFilesAfterEnv: [
-    '<rootDir>/projects/sw2e/src/test.ts'
+    '<rootDir>/projects/sw2e/src/test.ts',
+    '<rootDir>/projects/d6/src/test.ts'
   ],
   collectCoverage: true,
   coverageReporters: ['html'],
---
+++ projects/d6/src/lib/d6.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import {D6Component} from './d6.component';
+
+
+@NgModule({
+  declarations: [D6Component],
+  imports: [
+  ],
+  exports: [D6Component]
+})
+export class D6Module { }

0e4c3155320caa4aeaaa0300b403c5548d8f670c
Gabor Heves <hevesg@gmail.com>
2021-01-29 09:34:09 +0100

    implement proto form group into character sheet

--- projects/sw2e/src/app/character-sheet/character-sheet.component.ts
+++ projects/sw2e/src/app/character-sheet/character-sheet.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {ADI_GALLIA} from '../interface/mock/adi-gallia';
 
 @Component({
   selector: 'app-character-sheet',
@@ -7,12 +9,29 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CharacterSheetComponent implements OnInit {
 
+  public formGroup: FormGroup = this.getFormGroup();
+
+  public get name(): string {
+    return this.formGroup.get('name')?.value;
+  }
   constructor() { }
 
   ngOnInit(): void {
+    this.formGroup.setValue(ADI_GALLIA, { emitEvent: false });
   }
 
   onClickName(): void {
     console.log('hello');
+  }
+
+  private getFormGroup(): FormGroup {
+    return new FormGroup({
+      name: new FormControl(),
+      description: new FormControl(),
+      game: new FormControl('sw2e'),
+      characterPoints: new FormControl(),
+      attributes: new FormControl(),
+      force: new FormControl()
+    });
   }
 }

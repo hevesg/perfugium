86ef5c4e9aa671c722d6829a21179753614e6076
Gabor Heves <hevesg@gmail.com>
2023-03-05 16:39:34 +0100

    d6 pipe

--- projects/d6/src/lib/module/d6-pip/d6-pip.pipe.spec.ts
+++ projects/d6/src/lib/module/d6-pip/d6-pip.pipe.spec.ts
@@ -1,4 +1,4 @@
-import {D6PipPipe} from './d6-pip.pipe';
+import { D6PipPipe } from './d6-pip.pipe';
 
 describe('D6PipPipe', () => {
   const pipe: D6PipPipe = new D6PipPipe();
@@ -8,18 +8,18 @@ describe('D6PipPipe', () => {
   });
 
   it('return 0D if passed 0', () => {
-    expect(pipe.transform(0)).toEqual('0D');
+    expect(pipe.transform(0)).toEqual('0d');
   });
 
   it('return 0D+1 if passed 1', () => {
-    expect(pipe.transform(1)).toEqual('0D+1');
+    expect(pipe.transform(1)).toEqual('0d+1');
   });
 
   it('return 0D+2 if passed 2', () => {
-    expect(pipe.transform(2)).toEqual('0D+2');
+    expect(pipe.transform(2)).toEqual('0d+2');
   });
 
   it('return 1D if passed 3', () => {
-    expect(pipe.transform(3)).toEqual('1D');
+    expect(pipe.transform(3)).toEqual('1d');
   });
 });
--- projects/d6/src/lib/module/d6-pip/d6-pip.pipe.ts
+++ projects/d6/src/lib/module/d6-pip/d6-pip.pipe.ts
@@ -1,14 +1,33 @@
-import {Pipe, PipeTransform} from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
+/**
+ * Converts a <code>number</code> into a <code>string</code> representing a D6 pip
+ *
+ * You can chose to break one die in pips, i.e. You can transform 1d in three +1 pips or in one +1 and one +2.
+ *
+ * The gradation of skills/attributes works in this progression: 1d, 1d+1, 1d+2, 2d, 2d+1, 2d+2, 3d, 3d+1, 3d+2, 4d... And so on.
+ *
+ * Let's say if you have 3d+2 in Blasters, you can spend 1 pip (buying it with Characters Points) to increase it not to 3d+3 but to 4d.
+ */
 @Pipe({
   name: 'd6Pip'
 })
 export class D6PipPipe implements PipeTransform {
 
-  transform(value: number): unknown {
+  /**
+   * @example
+   * {{ 3 | d6Pip}} // returns 1d
+   * {{ 7 | d6Pip}} // returns 2d+1
+   * {{ 0 | d6Pip}} // returns 0d
+   *
+   * @param {number} value  The target to process see
+   * @returns The processed target number
+   */
+
+  transform(value: number): string {
     const dice: number = Math.floor(value / 3);
     const pips: number = value % 3;
-    return dice.toString() + 'D' + (!!pips ? '+' + pips : '');
+    return dice.toString() + 'd' + (!!pips ? '+' + pips : '');
   }
 
 }

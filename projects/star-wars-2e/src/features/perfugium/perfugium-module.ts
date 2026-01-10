fb836eeb72640b5564eb3e4f442624df4f93a6ff
Gabor Heves <hevesg@gmail.com>
2026-01-10 20:16:52 +0100

    perfugium module

--- projects/star-wars-2e/src/features/perfugium/perfugium-module.ts
+++ projects/star-wars-2e/src/features/perfugium/perfugium-module.ts
@@ -4,12 +4,12 @@ import { CharacterListComponent } from './components/character-list/character-li
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CharacterService } from './services/character.service';
 import { AgoPipe } from './pipes/ago/ago-pipe';
-import { ModalBaseComponent } from './components/modal-base/modal-base.component';
+import { ConfirmModalComponent } from './components/confirm-modal/confirm-modal.component';
 
 @NgModule({
-  declarations: [CharacterListComponent, NavbarComponent, AgoPipe, ModalBaseComponent],
+  declarations: [CharacterListComponent, NavbarComponent, AgoPipe, ConfirmModalComponent],
   imports: [CommonModule],
-  exports: [CharacterListComponent, NavbarComponent, AgoPipe, ModalBaseComponent],
+  exports: [CharacterListComponent, NavbarComponent, AgoPipe, ConfirmModalComponent],
   providers: [CharacterService],
 })
 export class PerfugiumModule {}
